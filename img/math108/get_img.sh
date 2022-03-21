1b1657e7af6144441c82b37f1315bf25f942fa56
Greg Albrecht <github.com@undef.net>
2022-03-21 11:53:55 -0700

    added Volcano-Ionosphere article

---
+++ img/math108/get_img.sh
@@ -0,0 +1,5 @@
+wget https://giro.uml.edu/common/img/giro-map-large.gif
+wget https://www.swpc.noaa.gov/sites/default/files/styles/medium/public/Ionosphere.gif
+wget https://www.weather.gov/images/jetstream/atmos/ionlayers.jpg
+wget https://planetary.s3.amazonaws.com/web/assets/pictures/20170815_ionosonde.png
+wget https://upload.wikimedia.org/wikipedia/commons/6/69/Ionogramme.png
